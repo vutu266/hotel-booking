17dd3e68faa4c0befe67fbaccf1a336fb89a9473
vutu266 <vutulsss@gmail.com>
2025-08-06 16:23:00 +0700

    adding logic for mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 
-const bookIcon = () => {
+const BookIcon = ()=> (
     <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
-    </svg>
-}
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 
 const Navbar = () => {
     const navLinks = [
@@ -48,9 +48,9 @@ const Navbar = () => {
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
                         </a>
                     ))}
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
+                    {user && <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
                         Dashboard
-                    </button>
+                    </button>}
                 </div>
 
                 {/* Desktop Right */}
@@ -60,12 +60,12 @@ const Navbar = () => {
                     {user ? 
                     (<UserButton>
                         <UserButton.MenuItems>
-                            <UserButton.Action label="My Bookings" labelIcon={<bookIcon/>} onClick={() => navigate('/my-bookings')}/>
+                            <UserButton.Action label="My Bookings" labelIcon={<BookIcon />} onClick={() => navigate('/my-bookings')}/>
                         </UserButton.MenuItems>
                     </UserButton>) 
                     : 
                     ( 
-                    <button onClick={openSignIn} className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                    <button onClick={openSignIn} className={`cursor-pointer px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
                         Login
                     </button>)
                     }
@@ -74,7 +74,16 @@ const Navbar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
+                    {user &&
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label="My Bookings" labelIcon={<BookIcon />} onClick={() => navigate('/my-bookings')}/>
+                        </UserButton.MenuItems>
+                    </UserButton>}
                     <img onClick={() => setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt='menu' className={`${isScrolled && 'invert'} h-4 cursor-pointer`}/>
+                    {!user && <button onClick={openSignIn} className={`px-5 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                        Login
+                    </button>}
                 </div>
 
                 {/* Mobile Menu */}
@@ -89,13 +98,13 @@ const Navbar = () => {
                         </Link>
                     ))}
 
-                    <button onClick={() => setIsMenuOpen(false)} className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                    {user && <button onClick={() => setIsMenuOpen(false)} className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
                         Dashboard
-                    </button>
+                    </button>}
 
-                    <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
-                    </button>
+                    </button>}
                 </div>
             </nav>
     );
